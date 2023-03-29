1d7f4d5052294be67a5a884b83762dec5e058a38
Melvz <Melvz@MELVZEU>
2023-03-29 17:30:41 +0800

    Test new commit

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -19,7 +19,7 @@ const Welcome = () => {
   return (
     <View>
       <View style={styles.container}>
-        <Text style={styles.userName}>Hello Melvz</Text>
+        <Text style={styles.userName}>Hello Melvz Soriano</Text>
         <Text style={styles.welcomeMessage}>Find your perfect job</Text>
       </View>
       <View style={styles.searchContainer}>
